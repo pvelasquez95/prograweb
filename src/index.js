09c3895be3f0e1aadced700423fd74788b4d454b
pvelasquez95 <pablovelasquez95@hotmail.com>
2018-09-18 22:17:32 -0600

    Version funcional, cambio a cards, pendiente img

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {PanelGroup,Panel,Button,ButtonToolbar,ListGroup,ListGroupItem} from 'react-bootstrap';
+import { PanelGroup, Panel, Button, ButtonToolbar, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { CardGroup, CardBody, CardTitle, CardImage, Card } from 'reactstrap';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,54 +10,59 @@ import './index.css';
 
 class Games extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        games: [
-          {name: "Dota 2", platform: ["PC"], rate: ["7"]},
-          {name: "Mario Odyssey", platform: ["NS"], rate: ["9.5"]},
-          {name: "Zelda Breath of the Wild", platform: ["NS"], rate: ["9"]},
-          {name: "Sea of Thieves", platform: ["XBOX"], rate: ["5"]},
-          {name: "State of Decay 2", platform: ["XBOX"], rate: ["4"]},
-          {name: "Overwatch", platform: ["PC"], rate: ["7.5"]},
-          {name: "Persona 5", platform: ["PS4"], rate: ["8.5"]},
-          {name: "Digimon Cyber Sleuth", platform: ["PS4"], rate: ["8"]},
-        ]
-      };
+        super(props);
+        this.state = {
+            games: [
+                { name: "Dota 2", platform: ["PC"], rate: ["7"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "Mario Odyssey", platform: ["NS"], rate: ["9.5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "Zelda Breath of the Wild", platform: ["NS"], rate: ["9"] , img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg']},
+                { name: "Sea of Thieves", platform: ["XBOX"], rate: ["5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "State of Decay 2", platform: ["XBOX"], rate: ["4"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "Overwatch", platform: ["PC"], rate: ["7.5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "Persona 5", platform: ["PS4"], rate: ["8.5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "Digimon Cyber Sleuth", platform: ["PS4"], rate: ["8"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+            ]
+        };
     }
     render() {
         const games = this.state.games;
-        return(
-          <div className="jumbotron">
-            <h1>Some of my loved videogames</h1>
-            <PanelGroup accordion id="games">
-              {games.map((game, index) => (
-                <Panel eventKey={index} key={index}>
-                  <Panel.Heading>
-                    <Panel.Title className="title" toggle>{game.name}</Panel.Title>
-                  </Panel.Heading>
-                  <Panel.Body collapsible>
-                    <ListGroup>
-                      {game.platform.map((platform, index) => (
-                        <ListGroupItem key={index}>{platform}</ListGroupItem>
-                      ))}
-                    </ListGroup>
-                    <ListGroup>
-                      {game.rate.map((rate, index) => (
-                        <ListGroupItem key={index}>{rate}</ListGroupItem>
-                      ))}
-                    </ListGroup>
-                    <ButtonToolbar>
-                      <Button bsStyle="warning">Edit</Button>
-                      <Button bsStyle="danger">Delete</Button>
-                    </ButtonToolbar>
-                  </Panel.Body>
-                </Panel>
-              ))}
-            </PanelGroup>
-            <Button bsStyle="primary">Add Game</Button>
-          </div>
+        return (
+            <div className="jumbotron">
+                <h1>All the videogames I've loved</h1>
+                <div id="games">
+                    {games.map((game, index) => (
+                        <Card key={index} className="col-md-6">
+                        {game.platform.map((img, index) => (
+                                            <CardImage top src={index}>{img} overlay="white-slight" hover waves alt="Card image cap"/>
+                                            </CardImage>  
+                                        ))}
+                            
+                            <CardBody>
+                                <CardTitle className="title">{game.name}</CardTitle>
+                                    <ListGroup>
+                                        {game.platform.map((platform, index) => (
+                                            <ListGroupItem key={index}>{platform}</ListGroupItem>
+                                        ))}
+                                    </ListGroup>
+                                    <ListGroup>
+                                        {game.rate.map((rate, index) => (
+                                            <ListGroupItem key={index}>{rate}</ListGroupItem>
+                                        ))}
+                                    </ListGroup>
+                                    <ButtonToolbar>
+                                        <Button bsStyle="warning">Edit</Button>
+                                        <Button bsStyle="danger">Delete</Button>
+                                    </ButtonToolbar>
+                                
+                            </CardBody>
+                            
+                        </Card>
+                    ))}
+                </div>
+                <Button bsStyle="primary">Add Game</Button>
+            </div>
         );
-      }
-    };
+    }
+};
 ReactDOM.render(<Games />, document.getElementById('app'));
 registerServiceWorker();

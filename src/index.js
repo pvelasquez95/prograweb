332c15056ce5fef710863f5d54df66708f83c87f
pvelasquez95 <pablovelasquez95@hotmail.com>
2018-09-18 23:07:10 -0600

    Version final fase 1, imagenes funcionando

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { PanelGroup, Panel, Button, ButtonToolbar, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { CardGroup, CardBody, CardTitle, CardImage, Card } from 'reactstrap';
+import { CardGroup, CardBody, CardTitle, CardImage, Card,ListItem } from 'reactstrap';
+import { CardHeader, CardFooter, ImageHeader  } from "react-simple-card";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -13,14 +14,14 @@ class Games extends React.Component {
         super(props);
         this.state = {
             games: [
-                { name: "Dota 2", platform: ["PC"], rate: ["7"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
-                { name: "Mario Odyssey", platform: ["NS"], rate: ["9.5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
-                { name: "Zelda Breath of the Wild", platform: ["NS"], rate: ["9"] , img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg']},
-                { name: "Sea of Thieves", platform: ["XBOX"], rate: ["5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
-                { name: "State of Decay 2", platform: ["XBOX"], rate: ["4"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
-                { name: "Overwatch", platform: ["PC"], rate: ["7.5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
-                { name: "Persona 5", platform: ["PS4"], rate: ["8.5"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
-                { name: "Digimon Cyber Sleuth", platform: ["PS4"], rate: ["8"], img:['https://res.cloudinary.com/teepublic/image/private/s--ZN1OWoLD--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1489237320/production/designs/1310293_1.jpg'] },
+                { name: "Dota 2", platform: ["PC"], rate: ["7"], photo: ['https://files.sirclocdn.xyz/kudouanimanga/products/_170803170241_21%20dota2_tn.jpg'] },
+                { name: "Mario Odyssey", platform: ["NS"], rate: ["9.5"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/8/8d/Super_Mario_Odyssey.jpg/220px-Super_Mario_Odyssey.jpg'] },
+                { name: "Zelda Breath of the Wild", platform: ["NS"], rate: ["9"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg/220px-The_Legend_of_Zelda_Breath_of_the_Wild.jpg'] },
+                { name: "Sea of Thieves", platform: ["XBOX"], rate: ["5"], photo: ['https://store-images.s-microsoft.com/image/apps.47844.14554784103656548.c603f1f6-32b9-41dc-8f67-41374fe8b6db.e1b20439-8be8-46e6-890a-f05a6cae6d1c?mode=crop&q=90&h=300&w=200'] },
+                { name: "State of Decay 2", platform: ["XBOX"], rate: ["4"], photo: ['https://i11d.3djuegos.com/juegos/13406/state_of_decay_2/fotos/ficha/state_of_decay_2-3753095.jpg'] },
+                { name: "Overwatch", platform: ["PC"], rate: ["7.5"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Overwatch_cover_art.jpg/220px-Overwatch_cover_art.jpg'] },
+                { name: "Persona 5", platform: ["PS4"], rate: ["8.5"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Persona_5_cover_art.jpg/220px-Persona_5_cover_art.jpg'] },
+                { name: "Digimon Cyber Sleuth", platform: ["PS4"], rate: ["8"], photo: ['https://k61.kn3.net/taringa/4/7/2/3/7/3//gonza491/330x330_0BB.jpg'] },
             ]
         };
     }
@@ -32,30 +33,30 @@ class Games extends React.Component {
                 <div id="games">
                     {games.map((game, index) => (
                         <Card key={index} className="col-md-6">
-                        {game.platform.map((img, index) => (
-                                            <CardImage top src={index}>{img} overlay="white-slight" hover waves alt="Card image cap"/>
-                                            </CardImage>  
-                                        ))}
-                            
                             <CardBody>
                                 <CardTitle className="title">{game.name}</CardTitle>
-                                    <ListGroup>
-                                        {game.platform.map((platform, index) => (
-                                            <ListGroupItem key={index}>{platform}</ListGroupItem>
-                                        ))}
-                                    </ListGroup>
-                                    <ListGroup>
-                                        {game.rate.map((rate, index) => (
-                                            <ListGroupItem key={index}>{rate}</ListGroupItem>
-                                        ))}
-                                    </ListGroup>
-                                    <ButtonToolbar>
-                                        <Button bsStyle="warning">Edit</Button>
-                                        <Button bsStyle="danger">Delete</Button>
-                                    </ButtonToolbar>
-                                
+                                <ListGroup>
+                                    {game.platform.map((platform, index) => (
+                                        <ImageHeader imageSrc={game.photo}/>
+                                    ))}
+                                </ListGroup>
+                                <ListGroup>
+                                    {game.platform.map((platform, index) => (
+                                        <ListGroupItem key={index}>{platform}</ListGroupItem>
+                                    ))}
+                                </ListGroup>
+                                <ListGroup>
+                                    {game.rate.map((rate, index) => (
+                                        <ListGroupItem key={index}>{rate}</ListGroupItem>
+                                    ))}
+                                </ListGroup>
+                                <ButtonToolbar>
+                                    <Button bsStyle="warning">Edit</Button>
+                                    <Button bsStyle="danger">Delete</Button>
+                                </ButtonToolbar>
+
                             </CardBody>
-                            
+
                         </Card>
                     ))}
                 </div>
